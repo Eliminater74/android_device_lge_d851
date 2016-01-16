67bd8dfbf37050d6f7b0b3ee2dcbddf939fb6024
Eliminater74 <eliminater74@gmail.com>
2016-01-16 10:52:07 -0500

    d851: Fixed for Pac Rom MM V6.0
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- pac.mk
+++ pac.mk
@@ -1,17 +1,15 @@
-PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+$(call inherit-product, device/lge/d855/full_d855.mk)
 
-$(call inherit-product, device/lge/d851/full_d851.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/pac/config/common_full_phone.mk)
+# Include PAC common configuration
+include vendor/pac/config/common_full_phone.mk
 
 # Enhanced NFC
 $(call inherit-product, vendor/pac/config/nfc_enhanced.mk)
 
-PRODUCT_DEVICE := d851
-PRODUCT_NAME := pac_d851
+PRODUCT_DEVICE := d855
+PRODUCT_NAME := pac_d855
 PRODUCT_BRAND := lge
-PRODUCT_MODEL := LG-D851
+PRODUCT_MODEL := LG-D855
 PRODUCT_MANUFACTURER := LGE
 
 # Bootanimation
@@ -19,6 +17,6 @@ PAC_BOOTANIMATION_NAME := 1440
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
-    PRODUCT_NAME="g3_tmo_us" \
-    BUILD_FINGERPRINT="lge/g3_tmo_us/g3:5.0.1/LRX21Y/15227151285fc:user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_tmo_us-user 5.0.1 LRX21Y 15227151285fc release-keys"
+    PRODUCT_NAME="g3_global_com" \
+    BUILD_FINGERPRINT="lge/g3_global_com/g3:6.0/MRA58K/2256973:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_global_com-user 6.0 MRA58K 2256973 release-keys"
